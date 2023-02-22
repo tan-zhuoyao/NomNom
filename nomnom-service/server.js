c090cb019c28e9f328370cb11f3f41342345368c
Tan Zhuo Yao <e0406711@u.nus.edu>
2023-02-22 13:00:57 +0800

    Add server get data handler

--- nomnom-service/server.js
+++ nomnom-service/server.js
@@ -6,8 +6,22 @@ dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, world from nomnom-service!');
+  res.send('Hello from nomnom-service!');
 });
 
+// just for testing now
+const data = {"1": "bob", "2": "alice"};
+
+// get nomnom posts filtered by userId
+app.get('/data/:userId', (req, res) => {
+  const { userId } = req.params;
+  if (!userId) {
+    res.status(400).send();
+  }
+  res.send(data);
+});
+
+
 const port = process.env.PORT || 3000;
+console.log("Server listening on: " + port);
 app.listen(port);
