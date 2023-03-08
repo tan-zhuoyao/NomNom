8409c00b4e95cd6369f38a11dbbd215286ec1f2e
xinyue <xinyue98.zhang@gmail.com>
2023-03-08 12:52:48 +0800

    Add material dashboard template

---
+++ nomnom-app/src/routes.js
@@ -0,0 +1,106 @@
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import BubbleChart from "@material-ui/icons/BubbleChart";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Notifications from "@material-ui/icons/Notifications";
+import Unarchive from "@material-ui/icons/Unarchive";
+import Language from "@material-ui/icons/Language";
+import Register from "@material-ui/icons/GroupAdd";
+import Login from "@material-ui/icons/LockOpen";
+// core components/views for Admin layout
+import HomePage from "views/Home/Home.jsx";
+import DashboardPage from "views/Dashboard/Dashboard.jsx";
+import UserProfile from "views/UserProfile/UserProfile.jsx";
+import TableList from "views/TableList/TableList.jsx";
+import Typography from "views/Typography/Typography.jsx";
+import Icons from "views/Icons/Icons.jsx";
+import Maps from "views/Maps/Maps.jsx";
+import NotificationsPage from "views/Notifications/Notifications.jsx";
+import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.jsx";
+// core components/views for Auth layout
+import LoginPage from "views/Pages/LoginPage.jsx";
+import RegisterPage from "views/Pages/RegisterPage.jsx";
+
+const dashboardRoutes = [
+  {
+    path: "/home",
+    name: "Home",
+    icon: Dashboard,
+    component: HomePage,
+    layout: "/admin"
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "/user",
+    name: "User Profile",
+    icon: Person,
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/table",
+    name: "Table List",
+    icon: "content_paste",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    icon: LibraryBooks,
+    component: Typography,
+    layout: "/admin"
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: BubbleChart,
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: LocationOn,
+    component: Maps,
+    layout: "/admin"
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: Notifications,
+    component: NotificationsPage,
+    layout: "/admin"
+  },
+  {
+    path: "/upgrade-to-pro",
+    name: "Upgrade To PRO",
+    icon: Unarchive,
+    component: UpgradeToPro,
+    layout: "/admin"
+  },
+  {
+    path: "/login-page",
+    name: "Login Page",
+    icon: Login,
+    component: LoginPage,
+    layout: "/auth"
+  },
+  {
+    path: "/register-page",
+    name: "Register Page",
+    icon: Register,
+    component: RegisterPage,
+    layout: "/auth"
+  }
+];
+
+export default dashboardRoutes;
