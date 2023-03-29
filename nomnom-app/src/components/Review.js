d45b4c15ec1994b4b1ff24ee18783fadbfd7942e
Tan Zhuo Yao <e0406711@u.nus.edu>
2023-03-29 15:44:21 +0800

    Add rating UI

--- nomnom-app/src/components/Review.js
+++ nomnom-app/src/components/Review.js
@@ -6,14 +6,24 @@ function Review(props) {
   const { userId, restaurant, review, rating, time, url } = props;
   const date = new Date(time);
   const dateFormat = date.getHours() + ":" + date.getMinutes() + ", " + date.toDateString();
-  console.log(dateFormat)
+
+  const ratingComponent = (rating) => {
+    let array = [];
+    for (let i = 0 ; i < rating; i++) {
+      array.push(<img id='star' alt="" src="star.png"></img>)
+    }
+    return array;
+  }
   return (
     <Card className='post-container'>
       <Card.Body>
         <Card.Subtitle id='post-date'>{dateFormat}</Card.Subtitle>
         <Card.Title id='post-restaurant'>{restaurant}</Card.Title>
         <Card.Subtitle id='post-username'>{userId}</Card.Subtitle>
-        <Card.Subtitle>Rating: {rating}/5</Card.Subtitle>
+        <div>
+          <div className='vertical-center' id='rating'>Rating: </div>
+          {ratingComponent(rating)}
+        </div>
         <Card.Text id='post-review'>{review}</Card.Text>
         {url && <Card.Img id='post-image' src={url}/>}
       </Card.Body>
